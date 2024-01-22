1351ddda80abc03659989201e92b32f238afb090
TH30D0R1D15 <48649894+TH30D0R1D15@users.noreply.github.com>
2024-01-23 00:10:47 +0100

    zufällige Frucht wird jetzt im Frucht-Modul erstellt

--- fruit.py
+++ fruit.py
@@ -2,15 +2,17 @@ import pygame
 import random
 class Fruit:
     def __init__(self, x, y, width, height, color):
-        self.x = random.randint(1, 1280 - 15)
-        self.y = random.randint(1, 720 - 15)
+        self.x = x
+        self.y = y
         self.width = width
         self.height = height
         self.color = color
 
-    def move(self, dx, dy):
-        self.x += dx
-        self.y += dy
+
+    def generate(self):
+        self.x = random.randint(10, 800 - 15)
+        self.y = random.randint(10, 600 - 15)
+
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
--- game.py
+++ game.py
@@ -1,24 +1,32 @@
+# Imports
 import pygame
 import random
-from snakePart import SnakePart  # Importiere die externe Klasse SnakePart
-from fruit import Fruit  # Importiere die externe Klasse Fruits
+from snakePart import SnakePart
+from fruit import Fruit
 
+#Hauptprogramm
 def main():
     # pygame setup
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((800, 600))
     clock = pygame.time.Clock()
 
     farbe = pygame.Color(255, 255, 255)
 
-    fruchtX = random.randint(5, 1280 - 15)
-    fruchtY = random.randint(5, 1280 - 15)
+
+    #fruchtX = random.randint(5, 1280 - 15)
+    #fruchtY = random.randint(5, 720 - 15)
+
+    fruchtX = random.randint(5, 800 - 15)
+    fruchtY = random.randint(5, 600 - 15)
 
 
+    # Erstelle ein Snake-Objekt
+    snake = [SnakePart(100, 100, 22.5, 22.5, (255, 0, 0))]
 
-    # Erstelle ein Block-Objekt
-    snake = [SnakePart(100, 100, 15, 15, (255, 0, 0))]
-    frucht = Fruit(fruchtX, fruchtY, 15, 15, (0, 255, 0))
+    # Erstelle ein Frucht-Objekt
+#    frucht = Fruit(fruchtX, fruchtY, 10, 10, (0, 255, 0))
+    frucht = Fruit(fruchtX, fruchtY, 10, 10, (0, 255, 0))
 
     running = True
 
@@ -27,39 +35,46 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+
+
+        # Tastatur-listening
+        #-------------------
         keys = pygame.key.get_pressed()
         dx = 0
         dy = 0
 
         if keys[pygame.K_LEFT]:
-            dx = -5
+            dx = -10
             dy = 0  # Keine vertikale Bewegung
 
         if keys[pygame.K_RIGHT]:
-            dx = 5
+            dx = 10
             dy = 0  # Keine vertikale Bewegung
 
         if keys[pygame.K_UP]:
-            dy = -5
+            dy = -10
             dx = 0  # Keine horizontale Bewegung
 
         if keys[pygame.K_DOWN]:
-            dy = 5
+            dy = 10
             dx = 0  # Keine horizontale Bewegung
 
 
         # Überprüfe, ob die Schlange die Frucht gegessen hat
         if snake[0].x == frucht.x and snake[0].y == frucht.y:
             # Die Schlange hat die Frucht gegessen, also generiere neue zufällige Koordinaten für die Frucht
-            frucht.x = random.randint(5, 1280 - 15)
-            frucht.y = random.randint(5, 720 - 15)
+            frucht.generate()
 
-
-        # Bewegen und Zeichnen des Blocks
+        # Bewegen und Zeichnen des Snake-Blocks
         for part in snake:
-
             part.move(dx, dy)
             part.draw(screen)
+
+        #  Zeichnen des Frucht-Blocks
+
+        frucht.draw(screen)
+
+
 
         pygame.display.flip()
         clock.tick(20)
